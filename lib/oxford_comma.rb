99fda4543b0133c5a64fc9012dc7d3b9f41c3501
Hieu <xuanhieu0012@gmail.com>
2021-09-21 15:54:39 -0500

    commit tree bf88b509965df74d32c0f304216b0e32cb6c0de9

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+    if array.length <= 1
+        array.join(",")
+    elsif array.length <= 2
+        array.join(" and ")
+    elsif array.length <= 3
+       last= array.pop
+       array.join(", ") << ", and #{last}"
+    elsif array.length > 3
+        last = array.pop
+        array.join(", ") << ", and #{last}"
+    end
 end
